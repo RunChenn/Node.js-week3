91c73b0fd1e180a6e3c4acaa2f2e02cfdc4a8d4a
runchen <yma8882003@gmail.com>
2022-05-03 14:13:09 +0800

    add controllers

---
+++ models/postsModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, '貼文姓名未填寫'],
+    },
+    content: {
+      type: String,
+      required: [true, 'Content 未填寫'],
+    },
+    tags: {
+      type: [String],
+    },
+    type: {
+      type: String,
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
---
+++ service/handleSuccess.js
@@ -0,0 +1,9 @@
+const handleSuccess = (res, data) => {
+  res.send({
+    status: 'success',
+    data: data,
+  });
+  res.end();
+};
+
+module.exports = handleSuccess;
