6d83644acbadd58694c403b5c01dbbaaf4e0c8e3
runchen <yma8882003@gmail.com>
2022-05-03 14:34:43 +0800

    add postman json

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Node.js-week3
+
+## 從 express MVC 架構建立 RESTful API
+
+- 將第二週程式碼改寫為 express 格式
+- routes： 將網址路徑管理拆到 routes 資料夾的 posts.js
+- model：將 post collections 拆到 model 資料夾，並載入到 routes/posts.js 上
--- connections/index.js
+++ connections/index.js
@@ -6,7 +6,6 @@ dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
 
 mongoose
-  // .connect('mongodb://localhost:27017/posts')
   .connect(DB)
   .then(() => {
     console.log('資料庫連線成功');
